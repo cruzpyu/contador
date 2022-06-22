1f5de81eea8679b4f6cf36687fa232cd7085b3d9
Yu <yulliepcruz@gmail.com>
2022-06-21 22:08:29 -0300

    adicionada função de desabilitar botões de incremento e decremento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,30 @@
 let incrementBtn = document.getElementById('increment');
 let decrementBtn = document.getElementById('decrement');
-
 let currentNumWrapper = document.getElementById('currentNum');
 let currentNum = 0;
-
 
 incrementBtn.addEventListener("click", function increment() {
     currentNum = currentNum + 1;
     currentNumWrapper.innerHTML = currentNum;
-    verificaLimite();
     if (currentNum > 0) {
         currentNumWrapper.style.color = '#E7AB79';
+    }
+    if (currentNum > 9) {
+        incrementBtn.disabled = true;
+    } else if (currentNum < 10) {
+        decrementBtn.disabled = false;
     }
 });
 
 decrementBtn.addEventListener("click", function decrement() {
     currentNum = currentNum - 1;
     currentNumWrapper.innerHTML = currentNum;
-    verificaLimite();
     if (currentNum < 0) {
         currentNumWrapper.style.color = '#B25068';
     }
-});
-
-function verificaLimite() {
-    if (currentNum > 9) {
-        incrementBtn.disabled = 'true';
-    } else if (currentNum < -9) {
-        decrementBtn.disabled = 'true';
+    if (currentNum < -9) {
+        decrementBtn.disabled = true;
+    } else if (currentNum > -10) {
+        incrementBtn.disabled = false;
     }
-}
+});
